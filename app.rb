65a8f3c2c14ff0bb63017a4c537e15e896d92ba3
carlostrapet <trapet_llamas@hotmail.com>
2018-02-05 15:18:30 +0000

    added params and cats

--- app.rb
+++ app.rb
@@ -16,9 +16,14 @@ get '/heather' do
   "Hiya!"
 end
 
-get '/cat' do
-  "<div style='border: 3px dashed red'>
-    <img src='http://bit.ly/1eze8aE'>
-  </div>"
-
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
 end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb(:index)
+end
+
